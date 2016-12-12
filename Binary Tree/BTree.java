68e46a446a8aa173e98716414edfe99aa3380898
yogendra saxena <yogendrasaxena56@gmail.com>
2016-12-12 13:46:06 +0530

    Update BTree.java

--- Binary Tree/BTree.java
+++ Binary Tree/BTree.java
@@ -18,10 +18,10 @@ class BTree
 	{
 		BTree tree = new BTree();
 
-		tree.root = new Node(1);
-		tree.root.left =new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.left.left = new Node(4);
+		root = new Node(1);
+		root.left =new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
 
 	}
 
