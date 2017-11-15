847fe93c90929f39c99f01c1499b792c1851702a
yogendra saxena <yogendrasaxena56@gmail.com>
2017-11-15 23:14:24 +0530

    Update All_Sorting.java

--- Sorting/All_Sorting.java
+++ Sorting/All_Sorting.java
@@ -1,4 +1,7 @@
-Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one. See following C implementation for details.
+Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves, calls itself for the
+two halves and then merges the two sorted halves. The merge() function is used for merging two halves. 
+The merge(arr, l, m, r) is key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two 
+sorted sub-arrays into one. See following C implementation for details.
 MergeSort(arr[], l,  r)
 If r > l
      1. Find the middle point to divide the array into two halves:  
@@ -9,7 +12,10 @@ If r > l
              Call mergeSort(arr, m+1, r)
      4. Merge the two halves sorted in step 2 and 3:
              Call merge(arr, l, m, r)
-The following diagram from wikipedia shows the complete merge sort process for an example array {38, 27, 43, 3, 9, 82, 10}. If we take a closer look at the diagram, we can see that the array is recursively divided in two halves till the size becomes 1. Once the size becomes 1, the merge processes comes into action and starts merging arrays back till the complete array is merged.
+The following diagram from wikipedia shows the complete merge sort process for an example array {38, 27, 43, 3, 9, 82, 10}.
+If we take a closer look at the diagram, we can see that the array is recursively divided in two halves till the size becomes 1
+Once the size becomes 1, the merge processes comes into action and starts merging arrays back till the complete array is
+merged.
 Merge-Sort
 
 
